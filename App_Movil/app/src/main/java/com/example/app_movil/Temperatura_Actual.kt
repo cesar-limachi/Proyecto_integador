d143d39b26bdfbba952d3f12c8da4e46090c3389
AlexanderRod <alexander.rodrigo.calderon@gmail.com>
2021-11-20 17:08:45 -0500

    App Movil

---
+++ App_Movil/app/src/main/java/com/example/app_movil/Temperatura_Actual.kt
@@ -0,0 +1,11 @@
+package com.example.app_movil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Temperatura_Actual : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperatura_actual)
+    }
+}
