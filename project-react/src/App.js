0168872e525f02e3d2dbd4e4ab9089ef888d91d4
cesar-limachi <limachi86@gmail.com>
2021-11-24 02:10:45 -0500

    tailwind install

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -3,22 +3,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>ESTE ES UN PARRAFO DE PRUEBA</p>
+    <div className="App w-full md:w-3/4 lg:w-5/2">
+        <ul className="space-y-4">
+      <li>
+        <div className="w-64 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-56 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-48 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-40 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-32 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-24 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-20 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-16 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-12 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+      <li>
+        <div className="w-10 h-3 bg-gradient-to-br from-fuchsia-500 to-purple-600"></div>
+      </li>
+    </ul>
+        
     </div>
   );
 }
